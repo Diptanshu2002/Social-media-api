2f248579cb8d24411d9db56fb4c4a773eb86a92b
Diptanshu <diptanshu2002@outlook.com>
2022-01-21 22:02:59 +0530

    added user update and delete operation

--- models/User.js
+++ models/User.js
@@ -41,6 +41,18 @@ const UserSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    desc:{
+      type: String,
+      max : 100,
+    },
+    city:{
+      type: String,
+      max : 50 
+    },
+    relationship :{
+      type : Number,
+      enum : [1 ,2 ,3],
+    }
   },
   {
     timestamps: true,
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,53 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
+const User = require('../models/User')
 
 // /api/users/
 
 router.get("/", (req ,res)=>{
     res.send("users")
 })
-
-
+//update user
+router.put('/:id' , async(req , res)=>{
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        //if password reset request came then encrypt the new password with bcrypt
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password , salt)
+            }catch(err){
+                res.status(500).json(err);
+            }
+        }
+        // updating user details to the database
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id , {$set : req.body});
+            res.status(200).json("account updated");
+        } catch (error) {
+            res.status(500).json("err")
+        }
+    }else{
+        res.status(403).json({error : "user data update failed"})
+    }
+})
+//delete user 
+router.delete('/:id' , async(req , res)=>{
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        
+        // deleting user from the database
+        try {
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("account deleted");
+        } catch (error) {
+            res.status(500).json("err")
+        }
+    }else{
+        res.status(403).json({error : "user data deletion failed"})
+    }
+})
+//get a user
+//follow a user
+//unfollow a user
 
 module.exports = router;
