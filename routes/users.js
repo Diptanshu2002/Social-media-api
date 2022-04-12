e1617244f6a278612571ad5edab8788e25856215
Diptanshu <diptanshu2002@outlook.com>
2022-04-12 15:28:10 +0530

    friends route has been added

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,16 @@ const bcrypt = require("bcrypt");
 const router = express.Router();
 const User = require('../models/User');
 const { Router } = require("express");
+const { verify } = require('../JWT/jwt')
 
 // /api/users/
 
-router.get("/", (req ,res)=>{
-    res.send("users")
-})
+// router.get("/", (req ,res)=>{
+//     res.send("users")
+// })
 
 //update user-----------------------------------------------------------------------------------------------------------------
-router.put('/:id' , async(req , res)=>{
+router.put('/:id',async(req , res)=>{
     if(req.body.userId === req.params.id || req.body.isAdmin){
         //if password reset request came then encrypt the new password with bcrypt
         if(req.body.password){
@@ -25,7 +26,6 @@ router.put('/:id' , async(req , res)=>{
         // updating user details to the database
         try {
             const user = await User.findByIdAndUpdate(req.params.id , {$set : req.body});
-            console.log(user);
             res.status(200).json("account updated");
         } catch (error) {
             res.status(500).json(error)
@@ -36,7 +36,7 @@ router.put('/:id' , async(req , res)=>{
 })
 
 //delete user---------------------------------------------------------------------------------------------------------------- 
-router.delete('/:id' , async(req , res)=>{
+router.delete('/:id', async(req , res)=>{
     if(req.body.userId === req.params.id || req.body.isAdmin){
         
         // deleting user from the database
@@ -52,11 +52,12 @@ router.delete('/:id' , async(req , res)=>{
 })
 
 //get a user------------------------------------------------------------------------------------------------------------------
-router.get('/:id' , async(req , res)=>{
+router.get('/',async(req , res)=>{
 
+        const userId = req.query.userId;
+        const username = req.query.username;
         try{
-            const user = await User.findById(req.params.id);
-            console.log(user)
+            const user = userId ? await User.findById(userId) : await User.findOne({username : username})
             const {password , updatedAt , ...other} = user._doc;
             res.status(200).json(other);
     
@@ -66,7 +67,7 @@ router.get('/:id' , async(req , res)=>{
 })
 
 //follow a user---------------------------------------------------------------------------------------------------------------
-router.put("/:id/follow", async (req,res)=>{
+router.put("/:id/follow",async (req,res)=>{
     if(req.body.userId !== req.params.id){
         try{
             const user = await User.findById(req.params.id);
@@ -74,7 +75,10 @@ router.put("/:id/follow", async (req,res)=>{
             if(!user.followers.includes(currentUser.id)){
                 await user.updateOne({$push : {followers : currentUser.id}});
                 await currentUser.updateOne({$push : {followings : user.id}});
-                res.status(200).json("used has been followed");
+                res.status(200).json({
+                    status:200,
+                    message:"Followed "+ user.email
+                });
             }else{
                 res.status(403).json("you already follow")
             }
@@ -109,6 +113,41 @@ router.put("/:id/unfollow", async (req,res)=>{
     }
 })
 
+router.get('/search', async(req, res)=>{
+    const query = req.query.q;
+    console.log(query);
+    try {
+        const queryResponse = await User.find({$email : {$search: `${query}`}})
+        // const queryResponse = await User.find({username : {$regex: /ribhu/}})
+        
+        res.json(queryResponse);
+    } catch (error) {
+        res.status(404).json('error')   
+    }
+})
+
+
+// getting all friends details----
+router.get('/friends/:id', async(req,res)=>{
+
+    try {
+        
+        const user = await User.findById(req.params.id);
+
+        const friends = await Promise.all(
+        user.followings.map(async(friend)=>{
+            friend = await User.findById(friend);
+            const {createdAt, updatedAt, password, refreshToken, ...restElements } = friend._doc
+            return restElements;    
+        })
+        )
+        res.status(200).json(friends)
+
+    } catch (error) {
+        console.log('/friends error', error)
+    }
+    
+})
 
 
 module.exports = router;
